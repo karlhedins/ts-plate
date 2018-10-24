50c09d86f94cde5abcd1c9c8bb2965a9fd36d796
Karl Hedin <karl.hedin@jayway.com>
2018-10-24 03:32:47 +0200

    style code blocks, fix copy paste button to be white

--- doczrc.js
+++ doczrc.js
@@ -1,4 +1,23 @@
+const style = `<style>button[title='Copy to clipboard'] svg { color: #fff; }</style>`;
+
 module.exports = {
   title: 'TS Plate',
   typescript: true,
-}
+  htmlContext: {
+    head: {
+      links: [
+        {
+          rel: 'stylesheet',
+          href: 'https://codemirror.net/theme/dracula.css',
+        },
+      ],
+      raw: [style],
+    },
+  },
+  themeConfig: {
+    codemirrorTheme: 'dracula',
+    colors: {
+      primary: '#077db2',
+    },
+  },
+};
