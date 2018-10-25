bc24377be99a7975370181476fa17ffa7e30b5b2
Karl Hedin <karl.hedin@jayway.com>
2018-10-25 17:55:55 +0200

    add fast setup section

--- doczrc.js
+++ doczrc.js
@@ -1,8 +1,19 @@
 const style = `<style>button[title='Copy to clipboard'] svg { color: #fff; }</style>`;
+import pkg from './package.json';
 
 module.exports = {
+  src: './src',
   title: 'TS Plate',
+  description: pkg.description,
   typescript: true,
+  menu: [
+    'Introduction',
+    {
+      name: 'Adding TS to a JS project',
+      menu: ['Fast setup'],
+    },
+    { name: 'Github', href: 'https://github.com/karlhedins/ts-plate' },
+  ],
   htmlContext: {
     head: {
       links: [
